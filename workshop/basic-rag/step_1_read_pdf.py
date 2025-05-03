f3299f579ae248d029c6e420fcfa61d0d195b54f
up1 <somkiat.p@gmail.com>
2025-05-03 22:14:04 +0700

    Read data from pdf file

---
+++ workshop/basic-rag/step_1_read_pdf.py
@@ -0,0 +1,19 @@
+from helper_utils import word_wrap
+from pypdf import PdfReader
+
+
+def read_pdf(file_path):
+    """Read a PDF file and return its text content."""
+    reader = PdfReader(file_path)
+    pdf_texts = [p.extract_text().strip() for p in reader.pages]
+
+    # Filter the empty strings
+    pdf_texts = [text for text in pdf_texts if text]
+    
+    return pdf_texts
+
+if __name__ == "__main__":
+    texts = read_pdf("./data/microsoft_annual_report_2022.pdf")
+    # Print the first text
+    print(word_wrap(texts[0]))
+    
