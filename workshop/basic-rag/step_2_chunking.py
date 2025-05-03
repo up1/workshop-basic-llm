139e731f8cd89ac3c88e3a28b2346eacbccca1bb
up1 <somkiat.p@gmail.com>
2025-05-03 22:16:45 +0700

    Step 2 chunking

---
+++ workshop/basic-rag/step_2_chunking.py
@@ -0,0 +1,24 @@
+from step_1_read_pdf import read_pdf
+from helper_utils import word_wrap
+from langchain.text_splitter import RecursiveCharacterTextSplitter, SentenceTransformersTokenTextSplitter
+
+
+def chunk_text(texts):
+    character_splitter = RecursiveCharacterTextSplitter(
+        separators=["\n\n", "\n", ". ", " ", ""],
+        chunk_size=1000,
+        chunk_overlap=0
+        )
+    character_split_texts = character_splitter.split_text('\n\n'.join(texts))
+    return character_split_texts
+
+if __name__ == "__main__":
+    # Read the PDF file
+    pdf_texts = read_pdf("./data/microsoft_annual_report_2022.pdf")
+    
+    # Chunk the text
+    chunk_texts = chunk_text(pdf_texts)
+    # Print the first chunk
+    print("First chunk:")
+    print(chunk_texts[0])
+    print(f"\nTotal chunks: {len(chunk_texts)}")
