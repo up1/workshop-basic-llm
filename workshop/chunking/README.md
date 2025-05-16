219785f9db7397516ebc9d4b6204f6f2c581c863
up1 <somkiat.p@gmail.com>
2025-05-17 00:57:25 +0700

    Demo witg fixed-size

---
+++ workshop/chunking/README.md
@@ -0,0 +1,14 @@
+# Workshop with chunking techniques
+* Library => [Agno](https://docs.agno.com/)
+* Chunking Techniques
+  * Fixed-size
+
+## 1. Install dependencies
+```
+$pip install -r requirements.txt
+```
+
+## 2. Fixed-size chunking
+```
+$python fixed_size_chunking.py
+```
---
+++ workshop/chunking/fixed_size_chunking.py
@@ -0,0 +1,18 @@
+from agno.agent import Agent
+from agno.document.chunking.fixed import FixedSizeChunking
+from agno.knowledge.pdf import PDFKnowledgeBase
+from agno.vectordb.chroma import ChromaDb
+
+knowledge_base = PDFKnowledgeBase(
+    path="data/ThaiRecipes.pdf",
+    vector_db=ChromaDb(collection="recipes", persistent_client=True),
+    chunking_strategy=FixedSizeChunking(),
+)
+knowledge_base.load(recreate=False)  # Comment out after first run
+
+agent = Agent(
+    knowledge=knowledge_base,
+    search_knowledge=True,
+)
+
+agent.print_response("How to make Som Tum?", markdown=True)
