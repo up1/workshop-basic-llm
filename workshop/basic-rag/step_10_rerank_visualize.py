50dc1a3a2997cfe3b9e799a95b9f5e77bc57e681
up1 <somkiat.p@gmail.com>
2025-05-17 00:34:41 +0700

    reranking

---
+++ workshop/basic-rag/step_10_rerank_visualize.py
@@ -0,0 +1,87 @@
+import chromadb
+from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
+from sentence_transformers import CrossEncoder
+import umap
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+
+def retrieve_from_vectordb(query):
+    """
+    Retrieve documents from the vector database based on the query.
+    """
+    try:
+        # Initialize the ChromaDB client
+        client = chromadb.PersistentClient(path="./db")
+
+        # Get the collection
+        embedding_function = SentenceTransformerEmbeddingFunction()
+        collection = client.get_collection("microsoft_annual_report_2022", embedding_function=embedding_function)
+
+        # query with scores and sort by distance
+        results = collection.query(query_texts=[query], n_results=10, include=["documents", "metadatas", "distances", "embeddings"])
+        retrieved_documents = results['documents'][0]
+        retrieved_embeddings = results['embeddings'][0]
+
+        # Rerank the results using a CrossEncoder
+        cross_encoder = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-12-v2')
+
+        # Re-rank and return the results
+        reranked_results = cross_encoder.predict([(query, doc) for doc in retrieved_documents], show_progress_bar=True)
+
+        # Merge results
+        # Sort the merged results and sort by reranked scores
+        sorted_indices = reranked_results.argsort()[::-1]
+        merged_results = {
+            "documents": [retrieved_documents[i] for i in sorted_indices],
+            "embeddings": [retrieved_embeddings[i] for i in sorted_indices],
+            "reranked_scores": [reranked_results[i] for i in sorted_indices]
+        }
+
+        # Get the embeddings for the entire collection
+        embeddings = collection.get(include=['embeddings'])['embeddings']
+        query_embedding = embedding_function([query])[0]
+        rerank_embeddings = merged_results['embeddings'][:5]
+
+        # Set up UMAP for dimensionality reduction
+        umap_transform = umap.UMAP(random_state=0, transform_seed=0).fit(embeddings)
+        projected_dataset_embeddings = project_embeddings(embeddings, umap_transform)
+        projected_query_embedding = project_embeddings([query_embedding], umap_transform)
+        projected_retrieved_embeddings = project_embeddings(rerank_embeddings, umap_transform)
+
+        # Visualize the results
+        visualize_results(projected_dataset_embeddings, projected_query_embedding, projected_retrieved_embeddings)
+
+        return merged_results
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        exit(1)
+
+def project_embeddings(embeddings, umap_transform):
+    umap_embeddings = np.empty((len(embeddings),2))
+    for i, embedding in enumerate(tqdm(embeddings)): 
+        umap_embeddings[i] = umap_transform.transform([embedding])
+    return umap_embeddings
+    
+def visualize_results(projected_dataset_embeddings, projected_query_embedding, projected_retrieved_embeddings):
+    plt.figure()
+    plt.scatter(projected_dataset_embeddings[:, 0], projected_dataset_embeddings[:, 1], s=10, color='gray')
+    plt.scatter(projected_query_embedding[:, 0], projected_query_embedding[:, 1], s=150, marker='X', color='r')
+    plt.scatter(projected_retrieved_embeddings[:, 0], projected_retrieved_embeddings[:, 1], s=100, facecolors='none', edgecolors='g')
+
+    plt.gca().set_aspect('equal', 'datalim')
+    plt.title(f'{query}')
+    plt.axis('off')
+    plt.show()
+    print("Visualizing results...")
+
+if __name__ == "__main__":
+    query = "What was the total revenue?"
+    results = retrieve_from_vectordb(query)
+    for result in results['reranked_scores']:
+        print("======================")
+        print("Reranked Result:")
+        print(result)
+        print('\n')
---
+++ workshop/basic-rag/step_9_rerank.py
@@ -0,0 +1,50 @@
+import chromadb
+from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
+from sentence_transformers import CrossEncoder
+
+def retrieve_from_vectordb(query):
+    """
+    Retrieve documents from the vector database based on the query.
+    """
+    try:
+        # Initialize the ChromaDB client
+        client = chromadb.PersistentClient(path="./db")
+
+        # Get the collection
+        embedding_function = SentenceTransformerEmbeddingFunction()
+        collection = client.get_collection("microsoft_annual_report_2022", embedding_function=embedding_function)
+
+        # query with scores and sort by distance
+        results = collection.query(query_texts=[query], n_results=10, include=["documents", "metadatas", "distances"])
+        retrieved_documents = results['documents'][0]
+
+        # Rerank the results using a CrossEncoder
+        cross_encoder = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-12-v2')
+
+        # Re-rank and return the results
+        reranked_results = cross_encoder.predict([(query, doc) for doc in retrieved_documents], show_progress_bar=True)
+
+        # Merge results['documents'][0] with results['distances'][0]
+        merged_results = {
+            "documents": retrieved_documents,
+            "reranked_scores": reranked_results
+        }
+        # Sort the merged results based on reranked scores
+        sorted_indices = reranked_results.argsort()[::-1]
+        merged_results["documents"] = [merged_results["documents"][i] for i in sorted_indices]
+        merged_results["reranked_scores"] = [merged_results["reranked_scores"][i] for i in sorted_indices]
+
+        return merged_results
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        exit(1)
+
+
+if __name__ == "__main__":
+    query = "What was the total revenue?"
+    results = retrieve_from_vectordb(query)
+    for result in results['documents']:
+        print("======================")
+        print("Reranked Result:")
+        print(result)
+        print('\n')
