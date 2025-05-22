a0008466b9ecd95339026298afd471b0e04ab2e8
up1 <somkiat.p@gmail.com>
2025-05-22 22:14:39 +0700

    Insert product data

--- workshop/basic_pgvector/db/init.sql
+++ workshop/basic_pgvector/db/init.sql
@@ -13,7 +13,18 @@ CREATE TABLE document_embeddings (
 CREATE INDEX document_embeddings_embedding_idx ON document_embeddings 
 USING hnsw (embedding vector_l2_ops);
 
+INSERT INTO documents (id, title) VALUES
+(1, 'Wireless Noise Cancelling Headphones'),
+(2, '4K Ultra HD Smart LED TV'),
+(3, 'Stainless Steel Electric Kettle'),
+(4, 'Bluetooth Portable Speaker'),
+(5, 'Ergonomic Office Chair'),
+(6, 'Smartphone with Triple Camera'),
+(7, 'Gaming Mechanical Keyboard'),
+(8, 'Fitness Activity Tracker'),
+(9, 'Home Espresso Coffee Machine'),
+(10, 'Robot Vacuum Cleaner');
 
-INSERT INTO documents VALUES ('1', 'The weather is lovely today.');
-INSERT INTO documents VALUES ('2', 'It s so sunny outside!');
-INSERT INTO documents VALUES ('3', 'He drove to the stadium.');
+-- INSERT INTO documents VALUES ('1', 'The weather is lovely today.');
+-- INSERT INTO documents VALUES ('2', 'It s so sunny outside!');
+-- INSERT INTO documents VALUES ('3', 'He drove to the stadium.');
