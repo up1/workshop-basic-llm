ed36a34643ac0e1d126b8d9721923c8ea7862a5c
Somkiat Puisungnoen <somkiat.p@gmail.com>
2025-10-22 11:09:32 +0700

    Update document_chunking.py

--- workshop/chunking/document_chunking.py
+++ workshop/chunking/document_chunking.py
@@ -1,18 +1,25 @@
+import asyncio
 from agno.agent import Agent
-from agno.document.chunking.document import DocumentChunking
-from agno.knowledge.pdf import PDFKnowledgeBase
+from agno.knowledge.chunking.document import DocumentChunking
+from agno.knowledge.knowledge import Knowledge
+from agno.knowledge.reader.pdf_reader import PDFReader
 from agno.vectordb.chroma import ChromaDb
 
-knowledge_base = PDFKnowledgeBase(
-    path="data/ThaiRecipes.pdf",
-    vector_db=ChromaDb(collection="recipes", persistent_client=True),
-    chunking_strategy=DocumentChunking(),
+knowledge_base = Knowledge(
+    vector_db=ChromaDb(collection="recipes", persistent_client=True)
 )
-knowledge_base.load(recreate=False)  # Comment out after first run
+
+asyncio.run(knowledge_base.add_content_async(
+    path="data/microsoft_annual_report_2022.pdf",
+    reader=PDFReader(
+        name="Document Chunking Reader",
+        chunking_strategy=DocumentChunking(),
+    )
+))
 
 agent = Agent(
     knowledge=knowledge_base,
     search_knowledge=True,
 )
 
-agent.print_response("How to make Som Tum?", markdown=True)
+agent.print_response("What was the total revenue of microsoft?", markdown=True)
